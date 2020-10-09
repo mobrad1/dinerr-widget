d488e72f419c6c2bea76b871625df410510e4fed
mobrad <yarrowbradley@gmail.com>
2020-10-09 14:46:13 +0100

    Added test key

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -162,7 +162,42 @@
                   <label for="inputState">State</label>
                   <select id="inputState" class="form-control" v-model="state" @change="selectArea($event)">
                     <option value="">Select your city</option>
-                    <option value="Lagos">Lagos</option>
+                    <option value="abia">Abia</option>
+                    <option value="adamawa">Adamawa</option>
+                    <option value="akwa_ibom">Akwa Ibom</option>
+                    <option value="anambra">Anambra</option>
+                    <option value="bauchi">Bauchi</option>
+                    <option value="bayelsa">Bayelsa</option>
+                    <option value="benue">Benue</option>
+                    <option value="borno">Borno</option>
+                    <option value="cross_river">Cross River</option>
+                    <option value="delta">Delta</option>
+                    <option value="ebonyi">Ebonyi</option>
+                    <option value="edo">Edo</option>
+                    <option value="ekiti">Ekiti</option>
+                    <option value="enugu">Enugu</option>
+                    <option value="gombe">Gombe</option>
+                    <option value="imo">Imo</option>
+                    <option value="jigawa">Jigawa</option>
+                    <option value="kaduna">Kaduna</option>
+                    <option value="kano">Kano</option>
+                    <option value="katsina">Katsina</option>
+                    <option value="kebbi">Kebbi</option>
+                    <option value="kogi">Kogi</option>
+                    <option value="kwara">Kwara</option>
+                    <option value="lagos">Lagos</option>
+                    <option value="nasarawa">Nasarawa</option>
+                    <option value="niger">Niger</option>
+                    <option value="ogun">Ogun</option>
+                    <option value="ondo">Ondo</option>
+                    <option value="osun">Osun</option>
+                    <option value="oyo">Oyo</option>
+                    <option value="plateau">Plateau</option>
+                    <option value="rivers">Rivers</option>
+                    <option value="sokoto">Sokoto</option>
+                    <option value="taraba">Taraba</option>
+                    <option value="yobe">Yobe</option>
+                    <option value="zamfara">Zamfara</option>
                   </select>
                 </div>
                 <div class="form-group col-md-6">
@@ -269,30 +304,7 @@ export default {
       areaDisabled: true,
       dinein : "delivery",
       disabled : false,
-      areas : {
-        "Lagos" : [
-           "Agege",
-            "Ajeromi-Ifelodun",
-            "Alimosho",
-            "Amuwo-Odofin",
-            "Apapa",
-            "Badagry",
-            "Epe",
-            "Eti Osa",
-            "Ibeju-Lekki",
-            "Ifako-Ijaiye",
-            "Ikeja",
-            "Ikorodu",
-            "Kosofe",
-            "Lagos Island",
-            "Lagos Mainland",
-            "Mushin",
-            "Ojo",
-            "Oshodi-Isolo",
-            "Shomolu",
-            "Surulere"
-        ]
-      },
+      areas : [],
       days : {
         sunday : [
             "2020-08-30",
@@ -761,7 +773,10 @@ export default {
         },
         selectArea(event){
          this.areaDisabled = false
-         this.selectAreas = this.areas[event.target.value]
+         this.axios.get('http://locationsng-api.herokuapp.com/api/v1/states/' + event.target.value + '/lgas').then(e =>{
+            this.selectAreas = e.data
+         })
+        
         },
         calculatePrice(event){
             this.$isLoading(true)
@@ -866,7 +881,7 @@ export default {
           
             let a = this;
             let handler = PaystackPop.setup({
-            key: 'pk_live_86416fab56ffa33012fdc7983191284c00b69fab', // Replace with your public key
+            key: 'pk_test_636c9a1c3f12f53b7812c201cc3abd07432eda85', // Replace with your public key
             email: this.email,
             amount: (this.total + this.delivery_fee) * 100,
             subaccount : this.restaurant.subaccount_code,
